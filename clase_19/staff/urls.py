3a0a81ffd92fe534e8a3e0e087150d70d24625bd
Francisco Eito <panchoeito@hotmail.com>
2023-03-19 14:41:52 -0300

    problema staff resuelto

--- clase_19/staff/urls.py
+++ clase_19/staff/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from staff import views
+from staff.views import inicio
 
 urlpatterns = [
     path('', inicio),
--- clase_19/staff/views.py
+++ clase_19/staff/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("Esta es la página de inicio")
+    return HttpResponse("Esta es la página de inicio", request)
+
+def index(request):
+    return render('staff/index.html', request)
